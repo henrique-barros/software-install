a22b2b44e584647091062de364c46bef74a952f2
henriquesb6 <henriquesb6@hotmail.com>
2015-11-05 09:47:04 -0200

    tirado senha

--- install-jboss.sh
+++ install-jboss.sh
@@ -1,12 +1,12 @@
 # Jboss - instalacao, permissao e incializacao do mesmo
 cd /tmp
-sudo wget --user=technosoftwarerp@gmail.com --password='/[technoRP780140]/'  https://bitbucket.org/technosoftware/technosoftware-scriptsinfra/raw/6098279e71a8f36e234175fe38d16af4c20d2dcc/wildfly-install.sh
+sudo wget
 dos2unix /tmp/wildfly-install.sh
 
 chmod 777 /tmp/wildfly-install.sh
 sudo /bin/bash /tmp/wildfly-install.sh
 
-wget --user=technosoftwarerp@gmail.com --password='/[technoRP780140]/'  https://bitbucket.org/technosoftware/technosoftware-scriptsinfra/raw/6098279e71a8f36e234175fe38d16af4c20d2dcc/wildfly-init-debian.sh
+wget
 sudo cp /tmp/wildfly-init-debian.sh /etc/init.d/wildfly
 sudo chown root:root /etc/init.d/wildfly
 sudo dos2unix /etc/init.d/wildfly
